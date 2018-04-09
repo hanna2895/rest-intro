cf87a57a0ebe0fedd11b0e387ac0e349e5acbe92
Hannah Werman <hannah.werman@gmail.com>
2018-04-09 14:46:16 -0500

    made an index route

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+const fruits = ['apple', 'banana', 'pear']
+
+// let's make an index route -> this will list all the fruits
+app.get('/fruits', (req, res) => {
+	res.send(fruits)
+})
+
 app.listen(3000, () => {
-	console.log("server listening on port 3000")
+	console.log("server listening on port 3000");
 })
