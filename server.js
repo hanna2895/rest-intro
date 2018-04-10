994678def168fc1ee3f21498871f8495e0e5c6fb
Hannah Werman <hannah.werman@gmail.com>
2018-04-10 10:26:43 -0500

    made a new template and a create route

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ const app = express();
 // our data
 const fruits = require('./models/fruits.js') // need a relative path to bring in a file -> different than syntax for module
 
+// these are all our ROUTES 
+
 // let's make an index route -> this will list all the fruits
 app.get('/fruits', (req, res) => {
-
-	// res.send(fruits);
 	res.render('index.ejs', {
 		theFruits: fruits, // <--data
 		pageTitle: "FRUITS INDEX"
 	})
 
+})
+
+// ** new ** route
+// this route will show a template for the user to add fruits
+// we put it here to avoid /fruits/:id hijacking it
+app.get('/fruits/new', (req, res) => {
+	res.render('new.ejs') // we are rendering a template
 })
 
 // *** SHOW *** route -> show all the info about one particular fruit
@@ -29,6 +36,12 @@ app.get('/fruits/:id', (req,res) => {
 		fruit: fruits[req.params.id]
 	})
 })
+
+app.post("/fruits", (req, res) => {
+	console.log(req)
+	res.send("you hit the post route")	
+})
+
 
 app.listen(3000, () => {
 	console.log("server listening on port 3000");
